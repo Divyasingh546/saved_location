c291ee6aad08e5ea745b02818ac519890d4a44df
Divya <divyakumarisingh546@gmail.com>
2023-09-02 12:32:08 +0530

    first commit

---
+++ Controllers/LocationController .cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Saved__Locations.Models;
+
+namespace Saved__Locations.Controllers
+{
+    [Route("api/location")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetLocation()
+        {
+            // Simulate retrieving location data (replace with your logic)
+            var location = new LocationInfo
+            {
+                Latitude = 40.7128, // Replace with actual latitude
+                Longitude = -74.0060, // Replace with actual longitude
+                Address = "New York, NY" // Replace with actual address
+            };
+
+            return Ok(location);
+        }
+    }
+
+}
---
+++ Models/LocationInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Saved__Locations.Models
+{
+    public class LocationInfo
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; }
+    }
+
+}
